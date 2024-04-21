6b05a351a20e5e7cf834763d9092c56af51cf830
olijag <oli-jag@wp.pl>
2024-04-21 22:10:01 +0200

    Added basic layout of the dashboard

--- main/FinanceTracker/views.py
+++ main/FinanceTracker/views.py
@@ -5,4 +5,17 @@ from django.template import loader
 
 def main(request):
     template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    
+    def format_usa_balance(number):
+        return f"{number:,.2f}"
+
+    spendings = 5326.00
+    income = 14074.00
+    balance = income - spendings
+
+    context = {
+        'spendings': format_usa_balance(spendings),
+        'income': format_usa_balance(income),
+        'balance': format_usa_balance(balance),
+    }
+    return HttpResponse(template.render(context, request))
