3e0d0225d0ac4c32338b83fb3de22d773cad2c58
olijag <oli-jag@wp.pl>
2024-04-23 20:13:57 +0200

    Added classes Transaction and Account

--- main/FinanceTracker/admin.py
+++ main/FinanceTracker/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Account, Transaction
 
 # Register your models here.
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("name", "balance")
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "amount", "date", "account")
+
+
+
+admin.site.register(Account, AccountAdmin)
+admin.site.register(Transaction, TransactionAdmin)
--- main/FinanceTracker/models.py
+++ main/FinanceTracker/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    balance = models.DecimalField(max_digits=9, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Transaction(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=40)
+    amount = models.DecimalField(max_digits=9, decimal_places=2)
+    date = models.DateField()
+    notes = models.CharField(max_length=255)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='transactions')
+
+    def __str__(self):
+        return f"{self.title} on {self.date} - Amount: {self.amount}"
+
+
