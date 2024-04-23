cee1a6e519259fd54edb9afcfe54928c59769179
olijag <oli-jag@wp.pl>
2024-04-23 21:03:55 +0200

    Implemented 'Last Transactions'

--- main/FinanceTracker/urls.py
+++ main/FinanceTracker/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.main, name="main"),
+    path("testing/", views.testing, name="testing")
 ]
--- main/FinanceTracker/views.py
+++ main/FinanceTracker/views.py
@@ -1,22 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Account, Transaction
 
-
-def main(request):
-    template = loader.get_template('index.html')
-    
-    def format_usa_balance(number):
+# Global functions 
+def format_usa_balance(number):
         number = float(number)
         return f"{number:,.2f}"
 
+
+# Main
+def main(request):
+    template = loader.get_template('index.html')
+
+    main_account = Account.objects.get(name="main")
+    transactions_main_account = main_account.transactions.all()    
+    balance = main_account.balance
+
     spendings = 5326.00
     income = 14074.00
-    balance = income - spendings
+    
 
     context = {
-        'spendings': format_usa_balance(spendings),
-        'income': format_usa_balance(income),
-        'balance': format_usa_balance(balance),
+        'main_account': main_account,
+        'transactions_main_account': transactions_main_account,
+        'balance': format_usa_balance(balance), 
     }
+    return HttpResponse(template.render(context, request))
+
+
+def testing(request):
+    template = loader.get_template('testing.html')
+    accounts = Account.objects.all().values()
+    
+    main_account = Account.objects.get(name="main")
+    transactions_main_account = main_account.transactions.all()
+
+
+    context = {
+         'accounts': accounts,
+         'main_account': main_account,
+         'transactions_main_account': transactions_main_account,
+    }  
+
     return HttpResponse(template.render(context, request))
